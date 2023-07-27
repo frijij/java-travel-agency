94f3eb1432363e9841a10aca46faa9b2d1944624
francesca <francipeco36@gmail.com>
2023-07-27 14:26:36 +0200

    exception added

--- src/org/java/travelagency/Holiday.java
+++ src/org/java/travelagency/Holiday.java
@@ -1,8 +1,12 @@
 package org.java.travelagency;
 
+import java.time.LocalDate;
+
 public class Holiday {
     // ATTRIBUTI: destinazione, data inizio, data fine
-
+private String destination;
+private LocalDate startOfHoliday;
+private LocalDate endOfHoliday;
 
     // COSTRUTTORI
 /*
@@ -12,6 +16,24 @@ Quando viene creata una nuova Vacanza vanno effettuati dei controlli:
 - la data fine non può essere prima della data inizio
 Se fallisce la validazione vanno sollevate opportune eccezioni
  */
+
+    public Holiday(String destination, LocalDate startOfHoliday, LocalDate endOfHoliday) throws RuntimeException{
+        if (destination == null || destination.isEmpty()) {
+            throw new RuntimeException("destination cannot be null or empty");
+        }
+
+        if (startOfHoliday == null || startOfHoliday.isBefore(LocalDate.now())) {
+            throw new RuntimeException("Holiday cannot start before the current date");
+        }
+
+        if (endOfHoliday == null || endOfHoliday.isBefore(startOfHoliday)){
+            throw new RuntimeException("Holiday cannot end before the starting date");
+        }
+
+        this.destination = destination;
+        this.startOfHoliday = startOfHoliday;
+        this.endOfHoliday = endOfHoliday;
+    }
 
     //GETTER E SETTER
 
