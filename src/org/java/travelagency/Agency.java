dad8c7004ad2e66eb648fd960e73c578d057adbb
francesca <francipeco36@gmail.com>
2023-07-27 14:16:36 +0200

    file preparation

---
+++ src/org/java/travelagency/Agency.java
@@ -0,0 +1,17 @@
+package org.java.travelagency;
+
+/* Chiediamo all’operatore se vuole inserire una nuova vacanza o uscire.
+Se vuole proseguire con l’inserimento va chiesta la destinazione, il giorno, mese e anno di partenza
+e il giorno, mese anno di ritorno.
+Con questi dati va generata una nuova vacanza e in console verrà stampato un messaggio del tipo:
+“Hai prenotato una vacanza di [numero di giorni] giorni a [destinazione] dal [data inizio formattata] al
+[data fine formattata]“.
+Se la creazione della vacanza genera un errore, il programma non deve interrompersi ma va gestito con
+dei messaggi di errore che permettono all’utente di capire cosa è andato storto e di ripetere l’inserimento.
+*/
+
+public class Agency {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/java/travelagency/Holiday.java
@@ -0,0 +1,22 @@
+package org.java.travelagency;
+
+public class Holiday {
+    // ATTRIBUTI: destinazione, data inizio, data fine
+
+
+    // COSTRUTTORI
+/*
+Quando viene creata una nuova Vacanza vanno effettuati dei controlli:
+- destinazione, data inizio e data fine non possono essere null
+- la data inizio non può essere già passata
+- la data fine non può essere prima della data inizio
+Se fallisce la validazione vanno sollevate opportune eccezioni
+ */
+
+    //GETTER E SETTER
+
+
+    //METODI
+    // metodo per calcolare la durata in giorni della vacanza
+
+}
