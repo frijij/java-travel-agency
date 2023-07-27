c3316bc05c13e72ed7f5cff38316b2a738885a5f
francesca <francipeco36@gmail.com>
2023-07-27 15:51:20 +0200

    class agency made

--- src/org/java/travelagency/Agency.java
+++ src/org/java/travelagency/Agency.java
@@ -29,19 +29,18 @@ public class Agency {
             switch (choice){
                 case "1":
                     // l'utente inserisce la destinazione e le date della sua vacanza
-                    String destination = null;
-                    LocalDate startOfHoliday = null;
-                    LocalDate endOfHoliday = null;
                     while (holiday == null) {
                         System.out.println("Choose a destination: ");
-                        destination = scan.nextLine();
+                        String destination = scan.nextLine();
                         System.out.println("From (yyyy-mm-dd): ");
+                        LocalDate startOfHoliday = null;
                         try {
                             startOfHoliday = LocalDate.parse(scan.nextLine());
                         } catch (Exception e) {
                             System.out.println("Invalid date format.");
                         }
                         System.out.println("To (yyyy-mm-dd): ");
+                        LocalDate endOfHoliday = null;
                         try {
                             endOfHoliday = LocalDate.parse(scan.nextLine());
                         } catch (Exception e) {
@@ -54,7 +53,6 @@ public class Agency {
                             System.out.println(e.getMessage());
                         }
                     }
-
                     System.out.println(holiday);
                     break;
                 case "2":
